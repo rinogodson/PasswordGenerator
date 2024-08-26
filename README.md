873241fcd72ca37b922af6a97b7e2bb30b9f84a2
Rino Godson <rinogodson@gmail.com>
2024-08-26 18:04:09 +0530

    🚀 Initial Commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Password Generator React App
+I'm now (August 26, 2024) a beginner React developer. Even though I've done a lot of projects in the past, I'm new to React.<br>This is one of my beginner projects.
+
+I'll explain how everything works in this project. 
+
---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [password, setPassword] = useState("");
+  const [capitals, setCapitals] = useState(true);
+  const [smallLetters, setSmallLetters] = useState(true);
+  const [numbers, setNumbers] = useState(true);
+  const [specialChars, setSpecialChars] = useState(true);
+  const [numOfChars, setNumOfChars] = useState(10);
+
+  function genpass(charCount, cap, small, num, special) {
+    let pass = "";
+    let charSet = "";
+    if (cap) {
+      charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (small) {
+      charSet += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (num) {
+      charSet += "0123456789";
+    }
+    if (special) {
+      charSet += "!@#$%^&*()_+";
+    }
+    for (let i = 0; i < charCount; i++) {
+      pass += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+    setPassword(pass);
+  }
+  return (
+    <>
+      <div className="container">
+        <div className="inputArea">
+          <input value={password} type="text" className="passOutput" />
+          <div className="checkboxes">
+            <input
+              value={numOfChars}
+              type="number"
+              className="numOfCharsInput"
+              onChange={(e) => {
+                setNumOfChars(e.target.value);
+              }}
+            />
+            <label>
+              <input
+                defaultChecked
+                type="checkbox"
+                className="capitalLetters"
+                onChange={(e) => {
+                  setCapitals(e.target.checked);
+                }}
+              />
+              Capital Letters
+            </label>
+            <label>
+              <input
+                defaultChecked
+                type="checkbox"
+                className="smallLetters"
+                onChange={(e) => {
+                  setSmallLetters(e.target.checked);
+                }}
+              />
+              Small Letters
+            </label>
+            <label>
+              <input
+                defaultChecked
+                type="checkbox"
+                className="numbers"
+                onChange={(e) => setNumbers(e.target.checked)}
+              />
+              Numbers
+            </label>
+            <label>
+              <input
+                defaultChecked
+                type="checkbox"
+                className="specialChars"
+                onChange={(e) => setSpecialChars(e.target.checked)}
+              />
+              Special Characters
+            </label>
+            <button
+              className="genPass"
+              onClick={() =>
+                genpass(
+                  numOfChars,
+                  capitals,
+                  smallLetters,
+                  numbers,
+                  specialChars
+                )
+              }
+            >
+              {" "}
+              Generate Password{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
